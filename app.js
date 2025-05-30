44e54460c4cc7aa568552d3f47dbf3bdcbe22a9e
Sarahleean <sarahsmithee119@gmail.com>
2025-05-30 11:01:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -190,6 +190,10 @@ const startServer = () => {
 };
 
 const port = process.env.PORT || 3000;
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
